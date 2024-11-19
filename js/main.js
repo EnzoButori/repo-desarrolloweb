b0e12eb615ac78e272ee05368d14a2c174218fd9
Enzo <enzobutoric@gmail.com>
2024-11-19 15:11:12 -0300

    v1.13

--- js/main.js
+++ js/main.js
@@ -3,22 +3,26 @@ const contadorProductos = document.getElementById('contador-productos');
 const totalPagar = document.querySelector('.total-pagar');
 const listaCarrito = document.querySelector('.container-cart-products');
 const iconoCarrito = document.querySelector('.icon-container');
+const checkbox = document.getElementById('check');
+const content = document.querySelector('.content');
+const botonesComprar = document.querySelectorAll('#btnComprar');
+const dbUrl = '../json/db.json';
 
 let carrito = {};
 let productos = [];
 
-// Comprobar si los elementos del carrito existen en el DOM
+// Mostrar/ocultar contenido del "More..."
+if (checkbox && content) {
+  checkbox.addEventListener('change', () => {
+    content.style.display = checkbox.checked ? 'block' : 'none';
+  });
+}
+
 if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoCarrito) {
-  const temporizadorHTML = document.createElement('p'); // Elemento para mostrar el temporizador
+  const temporizadorHTML = document.createElement('p');
   temporizadorHTML.classList.add('carrito-temporizador');
   listaCarrito.parentElement.appendChild(temporizadorHTML);
 
-  let temporizador = null;
-
-  // Ruta al archivo JSON
-  const dbUrl = '../json/db.json';
-
-  // Cargar productos desde el JSON
   async function cargarProductos() {
     try {
       const response = await fetch(dbUrl);
@@ -26,7 +30,8 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
         throw new Error('No se pudo cargar el archivo JSON');
       }
       const data = await response.json();
-      productos = data.productos; // Asegúrate de acceder correctamente a la clave `productos`
+      productos = data.productos;
+      asignarEventosBotones();
     } catch (error) {
       console.error('Error al cargar productos:', error);
     }
@@ -38,7 +43,6 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       carritoDropdown.style.display === 'none' || !carritoDropdown.style.display ? 'block' : 'none';
   });
 
-  // Agregar producto al carrito
   function agregarAlCarrito(event) {
     const button = event.target;
     const dataId = button.getAttribute('data-id');
@@ -55,13 +59,11 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       }
       actualizarCarrito();
       guardarCarritoEnLocalStorage();
-      if (!temporizador) iniciarTemporizador();
     } else {
       console.error(`Producto con data-id ${dataId} no encontrado`);
     }
   }
 
-  // Actualizar la visualización del carrito
   function actualizarCarrito() {
     listaCarrito.innerHTML = '';
     let total = 0;
@@ -71,6 +73,9 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       listaCarrito.innerHTML = '<p class="empty-cart-message">El carrito está vacío</p>';
       totalPagar.innerText = `$0.00`;
       contadorProductos.innerText = 0;
+
+      const cartTotal = document.querySelector('.cart-total');
+      if (cartTotal) cartTotal.remove();
       return;
     }
 
@@ -78,7 +83,7 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       const item = carrito[producto];
       total += item.precio * item.cantidad;
       totalItems += item.cantidad;
-  
+
       const cartItem = document.createElement('div');
       cartItem.classList.add('cart-product');
       cartItem.innerHTML = `
@@ -89,12 +94,11 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
         </div>
         <div class="quantity-buttons">
           <button class="quantity-button" onclick="event.stopPropagation(); quitarDelCarrito('${producto}')">-</button>
-          <button class="quantity-button" onclick="event.stopPropagation(); agregarAlCarrito('${producto}', ${item.precio})">+</button>
+          <button class="quantity-button" onclick="event.stopPropagation(); aumentarCantidad('${producto}')">+</button>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close" onclick="event.stopPropagation(); eliminarProducto('${producto}')">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-        </svg>`
-      ;
+        </svg>`;
       listaCarrito.appendChild(cartItem);
     }
 
@@ -107,45 +111,26 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       cartTotal.classList.add('cart-total');
       listaCarrito.appendChild(cartTotal);
     }
-  
+
     cartTotal.innerHTML = `
-    <h3>Total:</h3>
-    <span class="total-pagar">$${total.toFixed(2)}</span>
-    <div class="finalizar-compra">
-      <button id="openModal" class="boton botonModal" type="button">
-        <div class="fondo-boton"></div>
-        <h4>Finalizar Compra</h4>
-      </button>
-    </div>`
-  ;
+      <h3>Total:</h3>
+      <span class="total-pagar">$${total.toFixed(2)}</span>
+      <div class="finalizar-compra">
+        <button id="openModal" class="boton botonModal" type="button">
+          <div class="fondo-boton"></div>
+          <h4>Finalizar Compra</h4>
+        </button>
+      </div>`;
+
+      carritoDropdown.addEventListener('click', (event) => {
+        event.stopPropagation();
+      });
   }
 
-    carritoDropdown.addEventListener('click', (event) => {
-      event.stopPropagation();
-    });
-  }
-
-  // Guardar carrito en LocalStorage
   function guardarCarritoEnLocalStorage() {
     localStorage.setItem('carrito', JSON.stringify(carrito));
   }
 
-  // Asignar eventos a botones "Añadir al carrito"
-  function asignarEventosBotones() {
-    const botonesComprar = document.querySelectorAll('#btnComprar');
-    botonesComprar.forEach((boton) => {
-      boton.addEventListener('click', agregarAlCarrito);
-    });
-  }
-
-  // Cargar productos y carrito al iniciar la página
-  window.onload = async () => {
-    await cargarProductos();
-    cargarCarritoDesdeLocalStorage();
-    asignarEventosBotones();
-  };
-
-  // Cargar carrito desde LocalStorage
   function cargarCarritoDesdeLocalStorage() {
     const carritoGuardado = localStorage.getItem('carrito');
     if (carritoGuardado) {
@@ -153,6 +138,49 @@ if (carritoDropdown && contadorProductos && totalPagar && listaCarrito && iconoC
       actualizarCarrito();
     }
   }
+
+  function quitarDelCarrito(nombreProducto) {
+    if (carrito[nombreProducto]) {
+      carrito[nombreProducto].cantidad--;
+      if (carrito[nombreProducto].cantidad <= 0) {
+        delete carrito[nombreProducto];
+      }
+      actualizarCarrito();
+      guardarCarritoEnLocalStorage();
+    }
+  }
+
+  function aumentarCantidad(nombreProducto) {
+    if (carrito[nombreProducto]) {
+      carrito[nombreProducto].cantidad++;
+      actualizarCarrito();
+      guardarCarritoEnLocalStorage();
+    }
+  }
+
+  function eliminarProducto(nombreProducto) {
+    if (carrito[nombreProducto]) {
+      delete carrito[nombreProducto];
+      actualizarCarrito();
+      guardarCarritoEnLocalStorage();
+    }
+  }
+
+  function asignarEventosBotones() {
+    const botonesComprar = document.querySelectorAll('#btnComprar');
+    botonesComprar.forEach((boton) => {
+      boton.addEventListener('click', agregarAlCarrito);
+    });
+  }
+
+  window.onload = async () => {
+    await cargarProductos();
+    cargarCarritoDesdeLocalStorage();
+  };
+}
+
+
+
 
 
 
